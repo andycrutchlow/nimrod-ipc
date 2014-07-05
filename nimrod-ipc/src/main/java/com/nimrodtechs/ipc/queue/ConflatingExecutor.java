76eef6a0be9b59c98debb995afa2edf81c56370e
andy <crutchlowandy@gmail.com>
2014-07-05 08:18:45 -0400

    Added Apache License

--- nimrod-ipc/src/main/java/com/nimrodtechs/ipc/queue/ConflatingExecutor.java
+++ nimrod-ipc/src/main/java/com/nimrodtechs/ipc/queue/ConflatingExecutor.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Andrew Crutchlow
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.nimrodtechs.ipc.queue;
 
 import java.util.List;
@@ -7,7 +23,7 @@ import com.nimrodtechs.ipc.MessageReceiverInterface;
 
 /**
  * There should never be more than 2 entries in a queue...the one being worked
- * on and the latest arrived. Every message for same subject gets process
+ * on and the latest arrived. Every message for same subject gets processed
  * sequentially by same thread in the event there is a backlog... or delegate to
  * a new thread if there is no current thread for the subject.
  * Note : a wildcard subscription will be processed in one queue therefore will be subject to conflation, which might not be what
